73354bba3a0f6f60fa4bf4cdae28378ce517c987
t8rygy6mc7-maker <t8rygy6mc7@privaterelay.appleid.com>
2026-07-22 11:15:25 -0400

    Initial Commit

---
+++ budget Together/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  budget Together
+//
+//  Created by pulse nova on 7/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ budget Together/budget_TogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  budget_TogetherApp.swift
+//  budget Together
+//
+//  Created by pulse nova on 7/22/26.
+//
+
+import SwiftUI
+
+@main
+struct budget_TogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
